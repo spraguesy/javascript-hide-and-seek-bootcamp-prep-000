06c76a05ad9f80dfeb5d78c7280e9c65c587bfc8
Andrew Sprague <andrewsprague@outlook.com>
2018-07-17 19:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div div div div.target')
+}
+
+function increaseRankBy(n) {
+  var theList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i=0; i < theList.length; i++) {
+    var currRank = parseInt(theList[i].innerHTML);
+    currRank += n;
+    theList[i].innerHTML = currRank;
+  }
+}
+
+function deepestChild() {
+  var wholeList = document.querySelectorAll('div#grand-node');
+  
+  return wholeList
+}
