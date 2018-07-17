aa8656c2163734ae3821b3a593dbc673fb6415b9
Andrew Sprague <andrewsprague@outlook.com>
2018-07-17 20:09:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div div div div.target')
+}
+
+function increaseRankBy(n) {
+  var theList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i=0; i < theList.length; i++) {
+    var currRank = parseInt(theList[i].innerHTML);
+    currRank += n;
+    theList[i].innerHTML = currRank;
+  }
+}
+
+function deepestChild() {
+  var outside = document.querySelector('div#grand-node');
+  var inside = outside.querySelectorAll('div');
+  var wholeList  = []
+  
+  for (var i = 0; i < inside.length; i++) {
+    wholeList.push(inside[i]);
+  }
+  
+  var deepChild = wholeList[wholeList.length-1];
+  
+  return deepChild
+}
